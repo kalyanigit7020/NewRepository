aeb5d23378735ccd5cd28adeb7fe4d6a73a3586d
kalyanigit7020 <kalyanipawar746@gmail.com>
2023-02-21 15:10:50 +0530

    old

---
+++ src/com/kalyani/Test.java
@@ -0,0 +1,18 @@
+package com.kalyani;
+
+import java.lang.module.ModuleReader;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I am kalyani");
+	}
+
+	
+	ModuleReader Test() {
+		return null;
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Pawar
+ *
+ */
+module NewRepository {
+}
